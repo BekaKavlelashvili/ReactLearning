26ed76ecd4a6a625bdf003c26745a73751451827
Beka Kavlelashvili <beqakavlela2001@gmail.com>
2024-12-26 16:27:47 +0400

    class based components

---
+++ Class_Based_Components/src/components/UserFinder.js
@@ -0,0 +1,51 @@
+import { Fragment, useState, useEffect, Component } from "react";
+
+import Users from "./Users.js";
+import classes from "./UserFinder.module.css";
+import UsersContext from "../store/users-context.js";
+import ErrorBoundary from "./ErrorBoundary.js";
+
+class UserFinder extends Component {
+  static contextType = UsersContext;
+
+  constructor() {
+    super();
+    this.state = {
+      filteredUsers: [],
+      searchTerm: "",
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ filteredUsers: this.context.users });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <ErrorBoundary>
+          <Users users={this.state.filteredUsers} />
+        </ErrorBoundary>
+      </>
+    );
+  }
+}
+
+export default UserFinder;
